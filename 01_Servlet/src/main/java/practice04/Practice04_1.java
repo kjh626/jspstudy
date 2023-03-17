89adf92966b879df71159c77f6d2e4913fd47980
kjh626 <wjd1113@naver.com>
2023-03-17 12:18:28 +0900

    2023-03-17

--- 01_Servlet/src/main/java/practice04/Practice04_1.java
+++ 01_Servlet/src/main/java/practice04/Practice04_1.java
@@ -1,4 +1,4 @@
-package practice03;
+package practice04;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,15 +7,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Practice03_2")
-public class Practice03_2 extends HttpServlet {
+@WebServlet("/Practice04_1")
+public class Practice04_1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// 리다이렉트로 파라미터를 전달하려면 다시 파라미터를 붙여야 한다.
+		
 		String model = request.getParameter("model");
-		System.out.println(model);
-	
+		response.sendRedirect("/01_Servlet/Practice03_2?model=" + model);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- 01_Servlet/src/main/java/practice05/Practice05_1.java
+++ 01_Servlet/src/main/java/practice05/Practice05_1.java
@@ -1,22 +1,30 @@
-package practice03;
+package practice05;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Practice03_1")
-public class Practice03_1 extends HttpServlet {
+@WebServlet("/Practice05_1")
+public class Practice05_1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		// 리다이렉트로 파라미터를 전달하려면 다시 파라미터를 붙여야 한다.
+		// 냉장고 sysout으로 출력하고 Practice05_2로 리다이렉트
+		// 요청 인코딩
+		request.setCharacterEncoding("UTF-8");
 		
 		String model = request.getParameter("model");
-		response.sendRedirect("/01_Servlet/Practice03_2?model=" + model);
+		System.out.println("Practice05_1: " + model);
+		
+		// 응답할 URL의 인코딩이 필요하다.
+		response.sendRedirect("/01_Servlet/Practice05_2?model=" + URLEncoder.encode(model, "UTF-8"));
+		
 		
 	}
 
