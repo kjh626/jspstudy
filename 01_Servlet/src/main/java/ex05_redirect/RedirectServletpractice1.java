4ccbd388e34b9ffda1782b54c8e67b27f2b19a1d
kjh626 <wjd1113@naver.com>
2023-03-19 18:55:58 +0900

    2023-03-19

---
+++ 01_Servlet/src/main/java/ex05_redirect/RedirectServletpractice1.java
@@ -0,0 +1,27 @@
+package ex05_redirect;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RedirectServletpractice1")
+public class RedirectServletpractice1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String model = request.getParameter("model");
+		System.out.println("RedirectServletpractice1 : " + model);
+		
+		response.sendRedirect("/01_Servlet/RedirectServletpractice2");
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
