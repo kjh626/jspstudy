25182fdcd7bb4d945b5404e42a80e77776e606bd
kjh626 <wjd1113@naver.com>
2023-03-19 21:15:31 +0900

    2023-03-19

--- 01_Servlet/src/main/java/practice03/DataTimeServletpractice.java
+++ 01_Servlet/src/main/java/practice03/DataTimeServletpractice.java
@@ -1,6 +1,10 @@
 package practice03;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +16,29 @@ public class DataTimeServletpractice extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String type = request.getParameter("type");
+		
+		String result = null;
+		switch(type) {
+		case "1":
+			result = LocalDate.now().toString();
+			break;
+		case "2":
+			result = LocalTime.now().toString();
+			result = result.substring(0, result.indexOf("."));
+			break;
+		}
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<script>");
+		out.println("alert('요청결과는 " + result + "입니다.')");
+		out.println("</script>");
+		out.flush();
+		out.close();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
