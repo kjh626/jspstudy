32a0b47e24869426c5907eb943d4b2b5bc642f91
kjh626 <wjd1113@naver.com>
2023-03-19 19:17:01 +0900

    2023-03-19

--- 01_Servlet/src/main/java/practice01/Practice_p_01.java
+++ 01_Servlet/src/main/java/practice01/Practice_p_01.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Practice001")
-public class Practice001 extends HttpServlet {
+@WebServlet("/Practice_p_01")
+public class Practice_p_01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
