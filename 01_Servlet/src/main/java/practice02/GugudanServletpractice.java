9abc674d00638fcfa61cfaebe5b5db6c4b9bfcf9
kjh626 <wjd1113@naver.com>
2023-03-19 23:21:36 +0900

    2023-03-19

--- 01_Servlet/src/main/java/practice02/GugudanServletpractice.java
+++ 01_Servlet/src/main/java/practice02/GugudanServletpractice.java
@@ -1,6 +1,8 @@
 package practice02;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,39 @@ public class GugudanServletpractice extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		request.setCharacterEncoding("UTF-8");
 		
+		String strDan = request.getParameter("dan");
+		String strnum = request.getParameter("num");
+		String strResult = request.getParameter("result");
+		
+		int dan = 0, num =0, result = 0;
+		if(strDan != null && strDan.isEmpty() == false) {
+			dan = Integer.parseInt(strDan);
+		}
+		if(strnum != null && strnum.isEmpty() == false) {
+			num = Integer.parseInt(strnum);
+		}
+		if(strResult != null && strResult.isEmpty() == false) {
+			result = Integer.parseInt(strResult);
+		}
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<script>");
+		if(dan * num == result) {
+			out.println("alert('정답입니다.')");
+		} else {
+			out.println("alert('오답입니다.')");
+		}
+		//out.println("history.back()");
+		//out.println("location.href='/01_Servlet/practice02/clientprac1.html'");
+		out.println("location.href='/01_Servlet/practice02/clientprac2.html'");
+		out.println("</script>");
+		out.flush();
+		out.close();
 	
 	}
 
