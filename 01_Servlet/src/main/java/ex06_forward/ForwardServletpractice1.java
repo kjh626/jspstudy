743eb4a277cb5fc6effcd774f172cbd304378a01
kjh626 <wjd1113@naver.com>
2023-03-20 20:15:15 +0900

    2023-03-20

--- 01_Servlet/src/main/java/ex06_forward/ForwardServletpractice1.java
+++ 01_Servlet/src/main/java/ex06_forward/ForwardServletpractice1.java
@@ -12,6 +12,12 @@ public class ForwardServletpractice1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String model = request.getParameter("model");
+		System.out.println("ForwardServletpractice1 : " + model);
+		
+		request.getRequestDispatcher("/ForwardServletpractice2").forward(request, response);
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- 01_Servlet/src/main/java/ex06_forward/ForwardServletpractice2.java
+++ 01_Servlet/src/main/java/ex06_forward/ForwardServletpractice2.java
@@ -12,6 +12,9 @@ public class ForwardServletpractice2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String model = request.getParameter("model");
+		System.out.println("ForwardServletpractice2 : " + model);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
