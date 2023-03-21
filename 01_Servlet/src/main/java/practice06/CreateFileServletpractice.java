f5269ea33fbf675494b782d92dab31097911c75b
kjh626 <wjd1113@naver.com>
2023-03-21 16:09:20 +0900

    2023-03-21

--- 01_Servlet/src/main/java/practice06/CreateFileServletpractice.java
+++ 01_Servlet/src/main/java/practice06/CreateFileServletpractice.java
@@ -1,6 +1,12 @@
 package practice06;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +18,27 @@ public class CreateFileServletpractice extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String writer = request.getParameter("writer");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		String filename = LocalDate.now().toString() + "-" + writer + "-" + title + ".txt";
+		File dir = new File(request.getServletContext().getRealPath("storage"));
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		
+		File file = new File(dir, filename);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		bw.write(content);
+		bw.flush();
+		bw.close();
+		
+		response.sendRedirect("/01_Servlet/FileResponseServletpractice?filename=" + URLEncoder.encode(filename, "UTF-8"));
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- 01_Servlet/src/main/java/practice06/FileResponseServletpractice.java
+++ 01_Servlet/src/main/java/practice06/FileResponseServletpractice.java
@@ -1,6 +1,13 @@
 package practice06;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +19,19 @@ public class FileResponseServletpractice extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		request.setCharacterEncoding("UTF-8");
+		String filename = request.getParameter("filename");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("alert('" + filename + " 파일이 생성되었습니다.');");
+		out.println("location.href='/01_Servlet/practice06/clientprac.html';");
+		out.println("</script>");
+		out.flush();
+		out.close();
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
