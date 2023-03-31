3580a8e5d78afce4a40f636a179ac2da8c9b18d6
kjh626 <wjd1113@naver.com>
2023-03-31 12:29:52 +0900

    2023-03-31

---
+++ 03_MVC/src/main/java/model/AgeService.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgeService {
+
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		String strBirthyear = request.getParameter("birthyear");
+		int birthyear = Integer.parseInt(strBirthyear);
+		int nowyear = Calendar.getInstance().get(Calendar.YEAR);   // get(1) 이렇게 적은 거랑 똑같다. 1이라고 써줘야 연도가 나온다. 그래서 사람들이 생각하기 쉽게 필드값으로 만들어 놓은 것이다. 
+		
+		request.setAttribute("age", nowyear - birthyear);
+		return "view/output.jsp";
+	}
+	
+}
